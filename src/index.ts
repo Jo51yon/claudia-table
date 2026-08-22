8390c3c45340b4e6f8ad29bb8dea6fc6aaba4c43
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-22 20:53:07 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as ClaudiaTable } from './ClaudiaTable';
+export type { ClaudiaTableProps } from './ClaudiaTable';
+export type {
+  ClaudiaColumn, ClaudiaTableFilter, ClaudiaSort,
+  FetchPageParams, FetchPageResult, FetchPage,
+} from './types';
